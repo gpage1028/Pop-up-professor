4aa2e1baaab93e95d78129e751fac54734ced153
Giannte Page <Gianntepage@gmail.com>
2019-12-14 14:24:38 -0600

    New New

--- Pop up professor/ViewController.swift
+++ Pop up professor/ViewController.swift
@@ -10,21 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var QuestionTextField: UITextField!
+    @IBOutlet weak var AnswerTextField: UITextField!
+    
+    
+    @IBOutlet weak var DisplayTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        QuestionTextField.delegate = self
+        AnswerTextField.delegate = self
+        
+
         // Do any additional setup after loading the view.
     }
-        
-    @IBOutlet weak var QuestionTextField: UITextField!
-    
-    QuestionTextField.delegate = self
-    
+    //create action for button
+    @IBAction func EnterTapped(_ sender: Any) {
+   
+        DisplayTextView.text = "Question: \(QuestionTextField.text!)\nAnswer: \(AnswerTextField.text!)"
+
     }
-extension ViewController : UITextFieldDelegate {
-func textFieldShouldReturn(_ textField: UITextField) -> Bool {
     
-    textField.resignFirstResponder()
-    return true
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        AnswerTextField.resignFirstResponder()
+        
+    }
+    
+}
+
+extension ViewController : UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+}
 
     
 
