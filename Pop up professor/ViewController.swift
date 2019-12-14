18290f65b9397fca0a30255d79e5c69eb4be7b1c
Justin Lindberg <jlindber@bulldogs.aamu.edu>
2019-12-14 13:16:19 -0600

    "Added a note on ViewController to test

--- Pop up professor/ViewController.swift
+++ Pop up professor/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Justin Was Here
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
